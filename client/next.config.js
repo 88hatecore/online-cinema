e494612ffd55b3e73c82fd197771eb4c8ebda141
Ilya Dolgov <prhatecore@gmail.com>
2023-05-14 17:08:59 +0300

    setting the prettier and ts configurations

--- client/next.config.js
+++ client/next.config.js
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+	poweredByHeader: false,
+	env: {
+		APP_URL: process.env.REACT_APP_URL,
+		APP_ENV: process.env.REACT_APP_ENV,
+	},
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://localhost:4200/api/:path*",
+      },
+      {
+        source: "/uploads/:path*",
+        destination: "http://localhost:4200/uploads/:path*",
+      }
+    ]
+  }
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
